a0fbf654e9ff9f445a9b80382502a71fa0ca37b8
franr7 <franciscoromero071004@gmail.com>
2025-05-02 09:49:40 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "tramway.proxy.rlwy.net",
+  host: "turntable.proxy.rlwy.net",
   user: "root",
-  password: "kQFQPMPIkAojatyUTITqzDGeSfpWuYfg",
+  password: "rsllLXbWHEMvRUeLlIYsbvJmVPfJBfTV",
   database: "railway",
-  port: 35910,
+  port: 24490,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
